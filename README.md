2efc106086b1ed03e10d71d16a19392bf056d802
Nicola Prezza <nicola.prezza@unive.it>
2023-09-30 21:08:12 +0200

    new repo

---
+++ README.md
@@ -0,0 +1,37 @@
+Compute the substring complexity and related measures
+===============
+Author: Nicola Prezza (nicola.prezza@gmail.com)
+
+### Brief description
+
+This software computes the measure $\delta = max_k (d_k(k))$, where $d_k$ is the number of distinct factors of length $k$ of the input string. The tool outputs also related statistics, such as $argmax_k (d_k(k))$, $d_k/k$, and $d_k$ for small values of $k$.
+
+### Download
+
+To clone the repository, run:
+
+> git clone https://github.com/nicolaprezza/substring-complexity
+
+### Compile
+
+You need the SDSL library installed on your system (https://github.com/simongog/sdsl-lite).
+
+We use cmake to generate the Makefile. Create a build folder in the main folder:
+
+> mkdir build
+
+run cmake:
+
+> cd build; cmake ..
+
+and compile:
+
+> make
+
+### Run
+
+After compiling, run 
+
+>  delta file
+
+This command will output the statistics on the input string contained in the input file.
---
+++ delta.cpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2023, Nicola Prezza.  All rights reserved.
+// Use of this source code is governed
+// by a MIT license that can be found in the LICENSE file.
+
+#include <iostream>
+#include <vector>
+
+#include <iostream>
+#include <sdsl/lcp.hpp>
+#include <sdsl/suffix_arrays.hpp>
+
+using namespace std;
+using namespace sdsl;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " file" << endl;
+        return 1;
+    }
+    string file = argv[1];
+
+    cache_config cc(false); // do not delete temp files after csa construction
+    csa_wt<> csa;
+    construct(csa, file, 1);
+
+    cc.delete_files = true; // delete temp files after lcp construction
+    lcp_wt<> lcp;
+    construct(lcp, file, 1);
+
+    vector<int64_t> dk(lcp.size(),0);
+
+    //cout << "sa = "; for(auto x:csa) cout << x << " "; cout << endl;
+    //cout << "lcp = "; for(auto x:lcp) cout << x << " "; cout << endl;
+
+    for(uint64_t i=0;i<csa.size();++i){
+    
+    	dk[lcp[i]+1]++;
+    	if(csa[i]>0) dk[csa.size()-csa[i]+1]--;
+    
+    }
+
+    dk[1]--;// discard all kmers containing string terminator $
+
+    for(uint64_t i=1;i<dk.size();++i) dk[i] += dk[i-1];
+    
+    double delta = 0;
+	double argmax = 0;
+
+    for(uint64_t i=1;i<dk.size();++i){
+
+    	if(double(dk[i])/i>delta) argmax = i;
+
+    	delta = max(delta, double(dk[i])/i);
+
+    }
+
+
+    cout << "delta = " << delta << endl; 
+    cout << "argmax_k(d_k/k) = " << argmax << endl; 
+
+    cout << endl << "k\td_k\td_k/k" << endl;
+
+    for(int i=1;i<min(50,int(dk.size()));++i) 
+    	cout << i << "\t" << dk[i] << "\t" << double(dk[i])/i << endl;
+
+}
