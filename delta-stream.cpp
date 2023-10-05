24c70cdb4853e6ff90e068222f318a3ea3224dca
Nicola Prezza <nicola.prezza@unive.it>
2023-10-05 17:47:45 +0200

    parameters

--- delta-stream.cpp
+++ delta-stream.cpp
@@ -13,8 +13,9 @@ using namespace std;
 using namespace sdsl;
 using namespace hll;
 
+//template on the underlying count-distinct sketch
+template<class hll_t = HyperLogLogHIP>
 class sketch{
-
 
 	/*vector<uint64_t> v {1,2,2,2,3,4,5,5,5,6,6,7,7,7,7,8,9,9,9,10,10};
 	//HyperLogLog hll(10);
@@ -24,15 +25,23 @@ class sketch{
 
 public:
 
-	//total sketch size is _u * _p * 2^_r HLL registers
-	
-	static const uint8_t default_u = 32; 
-	static const uint64_t default_p = 100;
-	static const uint8_t default_r = 8;
+	//total sketch size is <= (_e + (_u/log2(a))) * 2^_r HLL registers 
+	//(_u/log2(a) = log_a(U), where U=2^_u is the upper bound to the stream's length)
+
+	static constexpr uint8_t default_u = 32; 
+	static constexpr double default_a = 1.001; //logarithm base for the sampling of factor lengths
+	static constexpr uint8_t default_r = 8;
+	static constexpr uint64_t default_e = 100; // the first default_e k-mer lengths are 1..default_e
 
 	sketch(	uint8_t u = default_u, 
+			double a = default_a,
 			uint8_t r = default_r, 
-			uint64_t p = default_p) : _u(u), _r(r), _p(p) {
+			uint64_t e = default_e) : _u(u), _a(a), _r(r), _e(e) {
+
+		//total number of HLL sketches: at least default_exact
+		//uint64_t number_hll_sketches = max(_u*_p,uint64_t(default_exact));
+
+		//hll_sketches = vector<hll_t>(number_hll_sketches,{_r});
 
 	}
 
@@ -76,18 +85,19 @@ public:
 		return _r;
 	}
 
-	//we use _n*get_precision() HLL sketches
-	uint64_t get_precision() const {
-		return _p;
-	}
 
 private:
 
-	uint8_t _u; // log2(upper bound to stream length)
-	uint8_t _r;  // log2(number of registers used by each HLL sketch)
-	uint64_t _p; // keep _p HLL sketches
+	vector<hll_t> hll_sketches;	// the count-distinct sketches
+	vector<uint64_t> lengths;  // sampled k-mer lengths corresponding to the sketches
 
-	uint64_t stream_length = 0; //current stream length
+	//current stream length (or sum of streams lengths if the sketch is the result of union of streams)
+	uint64_t stream_length = 0; 
+
+	uint8_t _u;  // log2(upper bound to stream length)
+	double _a;   //logarithm base for the sampling of factor lengths
+	uint8_t _r;  // log2(number of registers used by each HLL sketch)
+	uint64_t _e; // the first default_e k-mer lengths are 1..default_e
 
 };
 
@@ -98,6 +108,8 @@ private:
 void stream_delta(string outfile = {}){
 
 	cout << "file: " << outfile<<endl;
+
+	sketch<> s;
 
 
 }
@@ -119,18 +131,20 @@ int main(int argc, char* argv[]){
         		<< "		Store the resulting sketch to file s." << endl << endl
         		
         		<< "	-m s1 s2, --merge s1 s2" << endl
-        		<< "		Merge the sketches contained in files s1 and s2. Outputs delta(s1,s2). The two input sketches must have the same precision. Can be combined with -o to save the merged sketch." << endl << endl
+        		<< "		Merge the sketches contained in files s1 and s2. Outputs delta(s1,s2). The two input sketches must have the same parameters. Can be combined with -o to save the merged sketch." << endl << endl
 
         		<< "	-u u, --upper-bound u" << endl
-        		<< "		Logarithm in base 2 of the upper bound to the stream length (used with -s). The tool uses working space O(sqrt(2^u) * u). Default: u = " << sketch::default_u << "." << endl << endl
+        		<< "		Logarithm in base 2 of the upper bound to the stream length (used with -s). The tool uses working space O(sqrt(2^u) * u). Default: u = " << uint64_t(sketch<>::default_u) << "." << endl << endl
+
+        		<< "	-a a, --sample-rate a" << endl
+        		<< "		Sample rate. Samples log_a(stream length) factor lengths. Must be a double a>1. Default: a = " << sketch<>::default_a << "." << endl << endl
+
+        		<< "	-e E, --exact E" << endl
+        		<< "		Factor lengths {1,2,...,E} are always sampled. Default: E = " << sketch<>::default_e << endl << endl
+
 
         		<< "	-r R, --registers R" << endl
-        		<< "		Logarithm in base 2 of the number of registers used by each HLL sketch. Default: R = " << sketch::default_r << ". Range of N: [4,30]" << endl << endl
-
-        		<< "	-p P, --precision P" << endl
-        		<< "		Use P*(log n) HLL sketches (one per k-mer length, exponentially increasing k). Default: P = " << sketch::default_p << endl << endl
-
-
+        		<< "		Logarithm in base 2 of the number of registers used by each HLL sketch. Default: R = " << uint64_t(sketch<>::default_r) << ". Range of R: [4,30]" << endl << endl
 
         		<< endl;
         return 1;
